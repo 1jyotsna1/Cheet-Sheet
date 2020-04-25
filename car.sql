8fcb8e6238733ae31339cad041eadb8622707988
1jyotsna1 <42210760+1jyotsna1@users.noreply.github.com>
2020-04-25 21:04:52 +0530

    Add files via upload

---
+++ car.sql
@@ -0,0 +1,56 @@
+create table car (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	make VARCHAR(50) NOT NULL,
+	model VARCHAR(50) NOT NULL,
+	price NUMERIC(19,2) NOT NULL
+);
+insert into car (id, make, model, price) values (1, 'Toyota', 'RAV4', '94435.12');
+insert into car (id, make, model, price) values (2, 'Ford', 'Taurus X', '13842.64');
+insert into car (id, make, model, price) values (3, 'Ford', 'Explorer', '82362.29');
+insert into car (id, make, model, price) values (4, 'Chrysler', '300', '8519.03');
+insert into car (id, make, model, price) values (5, 'Cadillac', 'Escalade ESV', '1749.76');
+insert into car (id, make, model, price) values (6, 'Lexus', 'SC', '23276.32');
+insert into car (id, make, model, price) values (7, 'Chevrolet', 'Volt', '16014.47');
+insert into car (id, make, model, price) values (8, 'Dodge', 'Dakota Club', '94134.48');
+insert into car (id, make, model, price) values (9, 'Subaru', 'Impreza', '9321.38');
+insert into car (id, make, model, price) values (10, 'Toyota', '4Runner', '84271.93');
+insert into car (id, make, model, price) values (11, 'Mercedes-Benz', '400SE', '33061.62');
+insert into car (id, make, model, price) values (12, 'Toyota', 'Highlander Hybrid', '35519.61');
+insert into car (id, make, model, price) values (13, 'Mazda', 'Familia', '45933.48');
+insert into car (id, make, model, price) values (14, 'Jeep', 'Wrangler', '27560.89');
+insert into car (id, make, model, price) values (15, 'Buick', 'Century', '49791.49');
+insert into car (id, make, model, price) values (16, 'Lexus', 'SC', '58572.21');
+insert into car (id, make, model, price) values (17, 'Daewoo', 'Nubira', '32313.10');
+insert into car (id, make, model, price) values (18, 'Buick', 'Riviera', '57072.10');
+insert into car (id, make, model, price) values (19, 'Kia', 'Rio', '69821.41');
+insert into car (id, make, model, price) values (20, 'Toyota', 'Avalon', '28537.62');
+insert into car (id, make, model, price) values (21, 'GMC', 'Sierra 1500', '50901.22');
+insert into car (id, make, model, price) values (22, 'Mercedes-Benz', 'W123', '88820.22');
+insert into car (id, make, model, price) values (23, 'Pontiac', 'Firefly', '14717.37');
+insert into car (id, make, model, price) values (24, 'Mitsubishi', 'Challenger', '46458.49');
+insert into car (id, make, model, price) values (25, 'Buick', 'Riviera', '44737.63');
+insert into car (id, make, model, price) values (26, 'BMW', '5 Series', '61830.93');
+insert into car (id, make, model, price) values (27, 'Acura', 'RL', '66274.53');
+insert into car (id, make, model, price) values (28, 'Mitsubishi', '3000GT', '79564.85');
+insert into car (id, make, model, price) values (29, 'Cadillac', 'CTS', '2115.19');
+insert into car (id, make, model, price) values (30, 'Mercury', 'Cougar', '93089.69');
+insert into car (id, make, model, price) values (31, 'Mitsubishi', 'Eclipse', '93327.81');
+insert into car (id, make, model, price) values (32, 'Nissan', 'Armada', '43297.82');
+insert into car (id, make, model, price) values (33, 'GMC', 'Sierra 1500', '75696.81');
+insert into car (id, make, model, price) values (34, 'Ford', 'Windstar', '61477.02');
+insert into car (id, make, model, price) values (35, 'Chevrolet', 'Silverado 1500', '64071.31');
+insert into car (id, make, model, price) values (36, 'Suzuki', 'Swift', '16685.79');
+insert into car (id, make, model, price) values (37, 'Chrysler', 'Pacifica', '51981.42');
+insert into car (id, make, model, price) values (38, 'Geo', 'Prizm', '2735.70');
+insert into car (id, make, model, price) values (39, 'Land Rover', 'Range Rover', '76148.04');
+insert into car (id, make, model, price) values (40, 'Mazda', 'Mazda6', '43202.79');
+insert into car (id, make, model, price) values (41, 'Audi', 'A6', '69282.03');
+insert into car (id, make, model, price) values (42, 'Chevrolet', 'Suburban 1500', '78477.76');
+insert into car (id, make, model, price) values (43, 'Acura', 'TL', '56328.80');
+insert into car (id, make, model, price) values (44, 'Mercury', 'Mountaineer', '61745.10');
+insert into car (id, make, model, price) values (45, 'Ford', 'Transit Connect', '91581.39');
+insert into car (id, make, model, price) values (46, 'Acura', 'TL', '72947.71');
+insert into car (id, make, model, price) values (47, 'Buick', 'Roadmaster', '35548.67');
+insert into car (id, make, model, price) values (48, 'Ford', 'Probe', '59061.38');
+insert into car (id, make, model, price) values (49, 'Mercury', 'Capri', '54546.27');
+insert into car (id, make, model, price) values (50, 'Maserati', 'Quattroporte', '20023.77');
